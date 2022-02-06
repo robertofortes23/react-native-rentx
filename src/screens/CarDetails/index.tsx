de655c85dbba2761a82b1b1eae04931c9e28fe5f
Roberto Fortes <robertbernd23@gmail.com>
2022-02-06 19:23:43 -0300

    feat: details interface add-on

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import { StatusBar } from "react-native";
 
 import { BackButton } from "../../components/BackButton";
-import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header, CarImages } from "./styles";
+import {
+  Container,
+  Header,
+  Content,
+  CarImages,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from "./styles";
+import { ImageSlider } from "../../components/ImageSlider";
 
 export const CarDetails: React.FC = () => {
   const imageUrl =
@@ -14,9 +27,30 @@ export const CarDetails: React.FC = () => {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
       <CarImages>
         <ImageSlider imagesUrl={[imageUrl]} />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>{"Lamborguini"}</Brand>
+            <Name>{"Huracan"}</Name>
+          </Description>
+
+          <Rent>
+            <Period>{"Ao dia"}</Period>
+            <Price>{"R$ 580"}</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          {
+            "Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar."
+          }
+        </About>
+      </Content>
     </Container>
   );
 };
