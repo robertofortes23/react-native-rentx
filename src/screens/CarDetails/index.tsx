4aea6f1304bebf30e7da7fd82d1aea06336861ad
Roberto Fortes <robertbernd23@gmail.com>
2022-02-06 16:52:56 -0300

    feat: slider component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,65 +2,21 @@ import React from "react";
 import { StatusBar } from "react-native";
 
 import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
 
-import SpeedSvg from "../../assets/speed.svg";
-import AccelerationSvg from "../../assets/acceleration.svg";
-import ForceSvg from "../../assets/force.svg";
-import GasolineSvg from "../../assets/gasoline.svg";
-import ExchangeSvg from "../../assets/exchange.svg";
-import PeopleSvg from "../../assets/people.svg";
-
-import {
-  Container,
-  Header,
-  SliderContent,
-  Content,
-  Details,
-  Description,
-  Brand,
-  Name,
-  Rent,
-  Period,
-  Price,
-  About,
-  Accessorys,
-  Footer,
-} from "./styles";
+import { Container, Header, CarImages } from "./styles";
 
 export const CarDetails: React.FC = () => {
   const imageUrl =
     "https://www.freeiconspng.com/uploads/audi-png-transparent-png-12.png";
   return (
     <Container>
-      <StatusBar
-        translucent
-        backgroundColor="transparent"
-        barStyle="dark-content"
-      />
-
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
-
-      <Content>
-        <Details>
-          <Description>
-            <Brand>{"Lamborguini"}</Brand>
-            <Name>{"Huracan"}</Name>
-          </Description>
-
-          <Rent>
-            <Period>{"Ao dia"}</Period>
-            <Price>{"R$ 580"}</Price>
-          </Rent>
-        </Details>
-
-        <About>
-          {
-            "Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar."
-          }
-        </About>
-      </Content>
+      <CarImages>
+        <ImageSlider imagesUrl={[imageUrl]} />
+      </CarImages>
     </Container>
   );
 };
